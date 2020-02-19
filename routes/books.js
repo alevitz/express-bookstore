783690f2dbcdead3a4caa926cf4c8e95ad6fecf0
Adam <adamlevitz@gmail.com>
2020-02-18 16:44:22 -0800

    complete part 1

--- routes/books.js
+++ routes/books.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const Book = require("../models/book");
+const jsonschema = require("jsonschema");
+const bookSchema = require("../schemas/bookSchema.json");
+const ExpressError = require("../expressError")
+
 
 const router = new express.Router();
 
@@ -30,8 +34,17 @@ router.get("/:id", async function (req, res, next) {
 
 router.post("/", async function (req, res, next) {
   try {
+    const result = jsonschema.validate(req.body, bookSchema);
+    console.log(result);
+    if(!result.valid) {
+      let listOfErrors = result.errors.map(error => error.stack);
+      let error = new ExpressError(listOfErrors, 400);
+      return next(error);
+    } else {
+
     const book = await Book.create(req.body);
     return res.status(201).json({ book });
+    }
   } catch (err) {
     return next(err);
   }
